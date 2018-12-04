d06eb766d84c820fa297974bb4d33a00fc6b7371
heyhyungi <36056784+heyhyungi@users.noreply.github.com>
2018-12-03 20:35:17 -0700

    Update compargraph.php

--- EnergyDash/compargraph.php
+++ EnergyDash/compargraph.php
@@ -6,38 +6,22 @@
 new Chart(document.getElementById("line-chart"), {
 type: 'line',
 data: {
-labels: [
-  <?php
-      $conn = mysqli_connect("10.18.106.255", "testUser", "passpass", "testDB");
-
-      $query = 'SELECT Time_Of_Sample FROM 601sam100';
-      $result = mysqli_query($conn, $query);
-
-      while( $row = mysqli_fetch_array($result) ) {
-        echo '"';
-        echo $row['Time_Of_Sample'].'",';
-      }
-      mysqli_close($conn);
-    ?>
-],
+labels: ["00:00","01:00","02:00","03:00","04:00","05:00","06:00","07:00","08:00","09:00","10:00"],
 fillOpacity: .3,
 datasets: [{
-    data: [
-      <?php
-        $conn = mysqli_connect("10.18.106.255", "testUser", "passpass", "testDB");
-
-        $query = 'SELECT Sample_Value FROM 601sam100';
-        $result = mysqli_query($conn, $query);
-        while( $row = mysqli_fetch_array($result) ) {
-          echo $row['Sample_Value'] . ',';
-        }
-        mysqli_close($conn);
-      ?>],
+    data: [15203.04,15284.88,13507.83,14654.75,16106.85,15894.85,15989.33,14696.22,16431.25,13622,12053],
     label: "Week 1",
     borderColor: "#1f61a8",
     fill: origin,
     backgroundColor: "rgba(31,97,168,.3)",
-  }
+  },
+  {
+      data: [16509.04,15890.88,16288.83,16805.75,16082.85,16510.85,14714.33,15047.22,15737.25,16564,17394],
+      label: "Week 2",
+      borderColor: "#ffcc01",
+      fill: origin,
+      backgroundColor: "rgba(255,204,1,.3)",
+    }
 ]
 },
 options: {
