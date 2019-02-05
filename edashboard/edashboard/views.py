8cb1b153ebb34f7b24217fdbca56cb606ee54530
madison-boman <mrb475@nau.edu>
2019-02-04 18:59:33 -0700

    Fixed linking
    Fixed rearranging and the linking to pages.

--- edashboard/edashboard/views.py
+++ edashboard/edashboard/views.py
@@ -1,5 +1,24 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
+from django.views.generic import TemplateView
+from django.template import Template, Context
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the Energy Dashboard index.")
+
+def building_view(request):
+    return render(request, 'edashboard/building.html')
+
+def compare_view(request):
+    return render(request, 'edashboard/compare.html')
+
+def export_view(request):
+    return render(request, 'edashboard/export.html')
+
+def help_view(request):
+    return render(request, 'edashboard/help.html')
--- edashboard/mysite/urls.py
+++ edashboard/mysite/urls.py
@@ -1,29 +1,23 @@
 """edashboard URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
+    
+    The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
-Examples:
-Function views
+    Examples:
+    Function views
     1. Add an import:  from my_app import views
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
+    Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
+    Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+    """
 from django.contrib import admin
 from django.urls import include, path
-from . import views
-from .views import *
 
 app_name = 'edashboard'
 urlpatterns = [
-    path('admin/', admin.site.urls, name='admin'),
-    path('', views.index, name='index'),
-    path('building/', views.building_view, name='building'),
-    path('compare/', views.compare_view, name='compare'),
-    path('export/', views.export_view, name='export'),
-    path('help/', views.help_view, name='help'),
+               path('admin/', admin.site.urls),
+               path('edashboard/', include('edashboard.urls')),
 ]
