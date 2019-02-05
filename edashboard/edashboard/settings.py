89c191718fa869b41c34860fa51fb0076974f985
Ian Dale <idd6@nau.edu>
2019-02-04 17:53:13 -0700

    Changing Settings.py to include new dash app config file.

--- edashboard/edashboard/settings.py
+++ edashboard/edashboard/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-SETTINGS_PATH = os.path.normpath(os.path.dirname(__file__))
+#SETTINGS_PATH = os.path.normpath(os.path.dirname(__file__))
 
 
 # Quick-start development settings - unsuitable for production
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'dash.apps.DashConfig'
 ]
 
 MIDDLEWARE = [
