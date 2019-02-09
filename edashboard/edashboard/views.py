909f95136ee68c620e5b5b7f3352fc988dcf7dc7
madison-boman <mrb475@nau.edu>
2019-02-09 11:47:35 -0700

    Added Calculations
    Added calculations retrieved from epa.gov.
    Added the conversion code in comments to migrate from javascript to python later.
    Added more styling.

--- edashboard/edashboard/views.py
+++ edashboard/edashboard/views.py
@@ -22,3 +22,35 @@ def export_view(request):
 
 def help_view(request):
     return render(request, 'edashboard/help.html')
+
+'''FOR REFERENCE LATER
+#Pass in value and convert to kwH and convert based on string that conversion is.
+#CONVERSION LINK https://www.epa.gov/energy/greenhouse-gases-equivalencies-calculator-calculations-and-references
+def greenComp(value,conversion):
+
+    kwh = value
+    mtons = value*.0007
+    kilog=value*0.707
+    lbs=value*1.6
+    #BARRELS OF OIL 5.80 mmbtu/barrel × 20.31 kg C/mmbtu × 44 kg CO2/12 kg C × 1 metric ton/1,000 kg
+    if conversion=="oil":
+        return mtons/.43
+    #TREE'S GROWN FOR 10 YEARS
+    elif conversion=="tree":
+        return mtons/.06
+    #CARBON OFFSET
+    elif conversion=="carbon" :
+        return value
+    #Converts to Tons
+    elif conversion=="tons" :
+        return value*.0008
+    # Converts to Gas
+    elif conversion=="gas" :
+        return mtons/(8.887*(10 ** (-3)))
+    # CONVERTS BTU TO kWh
+    elif conversion=="btu" :
+        return kwh * 3412.14
+    # Converts to DOLLARS
+    else:
+        return kwh * 11.1
+'''
