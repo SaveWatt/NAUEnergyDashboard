9a2f8e4932d5c03b149c4093e5c8ab6b0d16c633
madison-boman <mrb475@nau.edu>
2019-02-18 14:14:22 -0700

    Export template updates
    Added the GUI for Export
    Added checks for fields to be null
    Added date picker with time
    Added graph data button
    Added download button

--- edashboard/mysite/settings.py
+++ edashboard/mysite/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
-import os
+import os,os.path
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -122,8 +122,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
-SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
-STATICFILES_DIRS = (
-  os.path.join(SITE_ROOT, 'static/'),
-)
