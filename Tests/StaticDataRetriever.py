ef2cb94f0bd45162d24d2b01c42b39ee5a130f71
Ian Dale <idd6@nau.edu>
2019-03-14 12:45:25 -0700

    Adjustments to fetching data.

--- Tests/StaticDataRetriever.py
+++ Tests/StaticDataRetriever.py
@@ -15,35 +15,88 @@ class StaticDataRetriever:
         self.__table = None
 
     def print_rows(self):
-        bnamelst = ['gamage','B1','blome','B2','1899','B3', 'adel','B26', 'allen hall','B46',
-        'athropology','B29', 'aquatic center','B49', 'ardey audit','B37A',
-        'ashurst','B11','beaver street school','B93','biology annex','B21B',
-        'bookstore','B35','bury','B5','bus barn', 'B83',
-        'calderon','B29','campus distribution cntr', 'B98D',
-        'campus heights','B50', 'centenial','B91', 'cline','B28',
-        'college of business','B81', 'college of engineering','B69',
-        'communication','B16', 'conference center','B58', 'cowden','B38',
-        'dubois','B64', 'dinning hall expansion','B30C', 'dlc','B16C',
-        'eastburn','B27', 'fieldhouse','B30', 'forestry','B82',
-        'gabaldon hall','B53', 'gateway','B43','geo annex','B13',
-        'geology','B12', 'gillenwaters hall','B31', 'health professions','B66',
-        'hilltop','B59', 'hlc','B25', 'honors hall','B41', 'hrm','B33',
-        'ihd','B27A', 'isp','B50A', 'its','B54', 'liberal arts','B18',
-        'mcconnell hall','B62', 'mcconnell suites','B75', 'mcdonald hall','B40',
-        'mckay village','B50B', 'morton hall','B4', 'mountain view hall','B55',
-        'nacc','B14', 'old main','B10', 'performing arts','B37',
-        'physical science','B19', 'pine ridge village','B95', 'raymond hall','B39',
-        'recital hall','B37', 'reilly hall','B48', 'res life warehouse','B98F',
-        'riles','B15', 'rosebury','B13A', 'san fran parking garage','B96B',
-        'SAS','B60', 'sbs east','B65', 'sbs west','B70', 'science & health','B36',
-        'science lab','B17', 'sechrist hall','B42', 'skydome','B73',
-        'skywiew','B87', 'south dinning','B64', 'south village apts','B71',
-        'student services','B30B', 'studnt servs vav overw','B30A',
-        'swing space','B98C', 'talor hall','B9', 'telcomm','B54A',
-        'tinsley','B44', 'univ dinning','B30D', 'univ services','B90',
-        'wettaw','B88', 'wilson','B45']
+        b_identifiers = {'B1': 'gammage',
+                         'B2': 'blome',
+                         'B3': 'north union',
+                         'B4': 'morton hall',
+                         'B8': 'bury',
+                         'B9': 'taylor hall',
+                         'B10': 'old main',
+                         'B11': 'ashurst',
+                         'B12': 'geology',
+                         'B13': 'geology annex',
+                         'B13A': 'rosebury appartments',
+                         'B14': 'native american cultural center',
+                         'B15': 'riles',
+                         'B16': 'communication',
+                         'B16A': 'distance learning center',
+                         'B17': 'nau lab',
+                         'B18': 'liberal arts',
+                         'B19': 'physical sciences',
+                         'B21B': 'biology annex',
+                         'B25': 'hlc',
+                         'B26': 'adel',
+                         'B27': 'eastburn',
+                         'B27A': 'ihd',
+                         'B28': 'cline',
+                         'B29': 'calderon',
+                         'B30': 'fieldhouse',
+                         'B30A': 'student services vav overview',
+                         'B30B': 'student services',
+                         'B30C': 'dining hall expansion',
+                         'B30D': 'university dining',
+                         'B31': 'gillenwater hall',
+                         'B33': 'hrm',
+                         'B35': 'bookstore',
+                         'B36': 'shb',
+                         'B37': 'performing arts',
+                         'B37A': 'ardrey auditorium',
+                         'B38': 'cowden hall',
+                         'B39': 'raymond hall',
+                         'B40': 'mcdonald hall',
+                         'B41': 'honors hall',
+                         'B42': 'sechrist hall',
+                         'B43': 'gateway',
+                         'B44': 'tinsley hall',
+                         'B45': 'wilson hall',
+                         'B46': 'allen hall',
+                         'B48': 'reilly hall',
+                         'B49': 'anthropology',
+                         'B50': 'campus heights',
+                         'B50A': 'isp',
+                         'B50B': 'mckay village',
+                         'B53': 'gabaldon hall',
+                         'B54': 'its',
+                         'B54A': 'telcomm',
+                         'B55': 'mountain view hall',
+                         'B58': 'conference center',
+                         'B59': 'hilltop',
+                         'B60': 'sas',
+                         'B62': 'mcconnell hall',
+                         'B64': 'dubois',
+                         'B65': 'sbs east',
+                         'B66': 'health professions',
+                         'B69': 'engineering',
+                         'B70': 'sbs west',
+                         'B71': 'south village apartments',
+                         'B73': 'skydome',
+                         'B75': 'mcconnell suites',
+                         'B81': 'business',
+                         'B82': 'forestry',
+                         'B83': 'bus barn',
+                         'B86': 'aquatic center',
+                         'B87': 'skyview',
+                         'B88': 'wettaw',
+                         'B90': 'nareh',
+                         'B91': 'parking services',
+                         'B93': 'beaver street school',
+                         'B95': 'pine ridge village',
+                         'B96B': 'san fran paraking garage',
+                         'B98C': 'swing space',
+                         'B98D': 'campus distribution center',
+                         'B98F': 'res life warehouse'}
 
-        #bnamelst1 = ['adel','B26', 'cline','B28', 'sas','B60',]
+        #bnamelst1 = ['adel','B26 ', 'cline','B28 ', 'sas','B60 ']
 
         cursor = self.__connection.cursor(as_dict=True)
         # Get list of trendlogs
@@ -51,15 +104,17 @@ class StaticDataRetriever:
         # Create a dictionary to store log information. Keys logdevnum_loginst
         log_dict = {}
         for row in cursor:
-            for name in bnamelst:
+            for num in b_identifiers.keys():
+                desc = row['logdescription'].upper()
                 # Searching for any instance of building name in all logdesc
-                if name.upper() in row['logdescription']:
+                #if num+" " in desc or num in desc or num[1:] in desc:
+                if 'B60 ' in desc or 'B60' in desc or 'SAS' in desc:
                     # Creating key
                     logdevnum = str(row['logdevnum'])
                     loginst = str(row['loginst'])
                     key = logdevnum+"_"+loginst
                     # Storing bname, logdesc, objname, TrendlogID in log_dict
-                    log_dict[key] = [name, row['logdescription'],
+                    log_dict[key] = ['B60', row['logdescription'],
                     row['objname'], row['TrendlogID']]
 
         # Querying for trendlogs based on log_dict info
@@ -76,20 +131,23 @@ class StaticDataRetriever:
                 logstring += "0"
             logstring += logid[1]
 
-            print(str(lognum) + ": logid: " + key + " logstring: " + logstring)
+            print(str(lognum) + ": logid: " + key + " logstring: " + logstring +
+            " building: " + log_dict[key][0])
+            print("--- " + log_dict[key][1])
             lognum +=1
 
             # Creating dynamic query for trendlog table
-            query = ('SELECT * FROM %s' % logstring)
+            #query = ('SELECT * FROM %s' % logstring)
             # Querying for trendlog table
-            cursor.execute(query, ())
+            #cursor.execute(query, ())
 
-            count = 0
-            for row in cursor:
-                if count > 3:
-                    break
-                print(row)
-                count+=1
+            #count = 0
+            #for row in cursor:
+            #    if count > 3:
+            #        break
+            #    print(row)
+            #    count+=1
+
         #self.dict_printer(log_dict)
 
     def dict_printer(self, dict):
