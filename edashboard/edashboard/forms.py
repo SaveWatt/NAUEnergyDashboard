34198d8970dfbff0cf594b136955d640197a7920
madison-boman <mrb475@nau.edu>
2019-04-01 18:23:32 -0700

    Pushing changes for 4/4 tasks
    -Login styling
    -Sign up styling

--- edashboard/edashboard/forms.py
+++ edashboard/edashboard/forms.py
@@ -21,7 +21,6 @@ class RegistrationForm(UserCreationForm):
         user.first_name = self.cleaned_data['first_name']
         user.last_name = self.cleaned_data['last_name']
         user.email = self.cleaned_data['email']
-
         if commit:
             user.save()
 
