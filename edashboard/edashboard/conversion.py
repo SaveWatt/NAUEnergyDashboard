0ab63b83e1346b856383fab377b00903b1e8d5ca
Hyungi Choi <hc294@nau.edu>
2019-04-23 15:39:18 -0700

    conversion
    update
    - units to dollar()

--- edashboard/edashboard/conversion.py
+++ edashboard/edashboard/conversion.py
@@ -1,19 +1,11 @@
-def kbtutobtu(data):
-    btu = round((data * 1000), 4)
-    return btu
+def kwtodollar(data):
+    dollar = round((data * 0.12), 4)
+    return dollar
 
-def kwtobtu(data):
-    btu = round((data / 0.00029307107), 4)
-    return btu
+def btutodollar(data):
+    dollar = round((data * 0.00029307107 * 0.12),4)
+    return dollar
 
-def gallonstobtu(data):
-    btu = round(((data/.7)*(8.887)), 4)
-    return btu
-
-def tonstobtu(data):
-    btu = round((data/.0008), 4)
-    return btu
-
-def dollorstobtu(data):
-    btu = round(((data/.00029307107)/0.12), 4)
-    return btu
+def gallontodollar(data):
+    dollar = round((data * 0.0015),4)
+    return dollar
