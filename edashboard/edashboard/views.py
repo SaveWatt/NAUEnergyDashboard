7048f9c7010d25e77eb7716b6cf7f619d1edf94c
Ian Dale <idd6@nau.edu>
2019-04-23 00:23:15 -0700

    Back End Updates
    - Removing redundant and unused code from views.py, models.py, and backend.py
    - New Model: SensorType to manage types and allow for new types in the future (Still in progress)
    - Add Sensor Types to Admin
    - Database Migrations

--- edashboard/edashboard/views.py
+++ edashboard/edashboard/views.py
@@ -1,33 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from http.server import BaseHTTPRequestHandler, HTTPServer
-from django.template import RequestContext
-from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import get_object_or_404, render
-from django.urls import reverse
-from django.views import generic
 import csv
-from django.contrib import auth
 from django.utils.encoding import smart_str
-from django.views.decorators.csrf import csrf_exempt
-from django.views.generic import TemplateView
-from django.template import Template, Context
-from django import template
 from edashboard.clean import *
 from edashboard.models import *
-from django.views import View
 from django.http import JsonResponse
 from edashboard.forms import *
-import json
-import time
 import statistics as stats
 import datetime
 from edashboard.backend import BackendRetriever
 from edashboard.backend import StaticDataRetriever as SDR
-from django.urls import reverse
-from urllib.parse import urlencode
 
-register = template.Library()
 sdr = SDR()
 BR = BackendRetriever()
 bname = BR.getBuildingStrings()
@@ -265,69 +248,3 @@ def login_view(request):
 def admin_view(request):
     buildings = BR.getBuildingStrings()
     return render(request, 'edashboard/admin.html',{'buildlist': buildings})
-'''
-def data(request):
-    db_data = Demo.objects.all().values_list('value', flat=True)
-    db_date = Demo.objects.all().values_list('date', flat=True)
-    return TemplateResponse(request, 'edashboard/export.html', {"db_data" : db_data})
-
-def data_com(request):
-    db_data = Demo.objects.all().values_list('value', flat=True)
-    db_date = Demo.objects.all().values_list('date', flat=True)
-    return TemplateResponse(request, 'edashboard/compare.html', {"db_data" : db_data})
-
-@register.building_name
-def getBuildingsName():
-    building = []
-    filename="buildings.csv"
-    with open(filename,'r') as f:
-        lines= f.readlines()
-        for line in lines:
-            if "number,name" not in line:
-                building.insert(len(building),line.rsplit(',', 1)[1])
-    f.close()
-    return building
-
-@register.building_num
-def getBuildingsNumber():
-    building = []
-    filename="buildings.csv"
-    with open(filename,'r') as f:
-        lines= f.readlines()
-        for line in lines:
-            if "number,name" not in line:
-                building.insert(len(building),line.rsplit(',', 1)[0])
-    f.close()
-    return building
-
-    FOR REFERENCE LATER
-    #Pass in value and convert to kwH and convert based on string that conversion is.
-    #CONVERSION LINK https://www.epa.gov/energy/greenhouse-gases-equivalencies-calculator-calculations-and-references
-    def greenComp(value,conversion):
-
-        kwh = value
-        mtons = value*.0007
-        kilog=value*0.707
-        lbs=value*1.6
-        #BARRELS OF OIL 5.80 mmbtu/barrel × 20.31 kg C/mmbtu × 44 kg CO2/12 kg C × 1 metric ton/1,000 kg
-        if conversion=="oil":
-            return mtons/.43
-        #TREE'S GROWN FOR 10 YEARS
-        elif conversion=="tree":
-            return mtons/.06
-        #CARBON OFFSET
-        elif conversion=="carbon" :
-            return value
-        #Converts to Tons
-        elif conversion=="tons" :
-            return value*.0008
-        # Converts to Gas
-        elif conversion=="gas" :
-            return mtons/(8.887*(10 ** (-3)))
-        # CONVERTS BTU TO kWh
-        elif conversion=="btu" :
-            return kwh * 3412.14
-        # Converts to DOLLARS
-        else:
-            return kwh * 11.1
-    '''
